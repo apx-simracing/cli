ad3ab22816781c195dcc88ffad480b2edacb899d
cmllr <fury224@googlemail.com>
2020-12-20 21:24:12 +0100

    Typo fix

--- commands/status.py
+++ commands/status.py
@@ -34,7 +34,7 @@ def get_drivers_command(env, *args, **kwargs):
                     "vehicle": v["vehicleName"],
                     "penalties": v["penalties"],
                     "pitstops": v["pitstops"],
-                    "lapDistannce": v["lapDistance"],
+                    "lapDistance": v["lapDistance"],
                     "vehicleClass": v["carClass"],
                     "steamID": v["steamID"] if v["steamID"] != 0 else None
                 }, vehicles))
