489d22379f9080050b1a73152b7add71b3df02b5
cmllr <fury224@googlemail.com>
2021-01-15 19:02:05 +0100

    Added unlock command, applied format

--- commands/commands.py
+++ commands/commands.py
@@ -2,11 +2,38 @@ from requests import post
 
 from commands.env import stop_command, list_command, oneclick_start_command
 from commands.status import get_status_command, get_drivers_command, get_states_command
-from commands.chat import chat_command, kick_command, add_bot_command, restart_weekend_command,  restart_race_command, restart_race_command, next_session_command, rejoin_driver_command
-from commands.deployment import deploy_command, install_command, unlock_command
-from commands.build import build_skin_command, get_config_command, get_ports_command, stamp_command
-from commands.results import get_results_command, process_results_command, update_results_command
-from commands.util import get_rfcmp_info_command, get_components_in_directory_command, check_config_command
+from commands.chat import (
+    chat_command,
+    kick_command,
+    add_bot_command,
+    restart_weekend_command,
+    restart_race_command,
+    restart_race_command,
+    next_session_command,
+    rejoin_driver_command,
+)
+from commands.deployment import (
+    deploy_command,
+    install_command,
+    unlock_command,
+    get_lockfile_command,
+)
+from commands.build import (
+    build_skin_command,
+    get_config_command,
+    get_ports_command,
+    stamp_command,
+)
+from commands.results import (
+    get_results_command,
+    process_results_command,
+    update_results_command,
+)
+from commands.util import (
+    get_rfcmp_info_command,
+    get_components_in_directory_command,
+    check_config_command,
+)
 
 SHELL_COMMANDS = {
     "status": get_status_command,
@@ -30,11 +57,12 @@ SHELL_COMMANDS = {
     "ports": get_ports_command,
     "install": install_command,
     "unlock": unlock_command,
+    "lockfile": get_lockfile_command,
     "rfcmpinfo": get_rfcmp_info_command,
     "rfcmpdir": get_components_in_directory_command,
     "checkconfig": check_config_command,
     "updateresults": update_results_command,
-    "stamp": stamp_command
+    "stamp": stamp_command,
 }
 
 
--- commands/deployment.py
+++ commands/deployment.py
@@ -29,18 +29,20 @@ def deploy_command(env, *args, **kwargs) -> bool:
         if "conditions" in json_data:
             if "weather" in json_data["conditions"]:
                 weather_filename = json_data["conditions"]["weather"]
-                upload_files['weather'] = open(weather_filename, "r").read()
+                upload_files["weather"] = open(weather_filename, "r").read()
                 if "grip" in json_data["conditions"]:
                     grip = {}
-                    for grip_session, grip_filename in json_data["conditions"]["grip"].items():
-                        upload_files[grip_session] = open(
-                            grip_filename, "rb").read()
+                    for grip_session, grip_filename in json_data["conditions"][
+                        "grip"
+                    ].items():
+                        upload_files[grip_session] = open(grip_filename, "rb").read()
 
         got = post(
-            url + "/deploy", headers={"authorization": secret},
-            data={"config": data, "rfm_config": open(
-                rfm_filename, "r").read()},
-            files=upload_files)
+            url + "/deploy",
+            headers={"authorization": secret},
+            data={"config": data, "rfm_config": open(rfm_filename, "r").read()},
+            files=upload_files,
+        )
 
         result = got.status_code == 200
     return result
@@ -55,7 +57,7 @@ def install_command(env, *args, **kwargs) -> bool:
     status_json = loads(running_text)
     if "not_running" not in status_json:
         raise Exception("Server is running, install failed")
-    got, text = http_api_helper(env, "install",  {}, get)
+    got, text = http_api_helper(env, "install", {}, get)
     print(text)
     return got
 
@@ -70,4 +72,23 @@ def unlock_command(env, *args, **kwargs):
         secret = server_data["secret"]
         file = args[0][0]
         got = post(
-            url + "/unlock", headers={"authorization": secret}, files={'unlock': open(file, 'rb')})
+            url + "/unlock",
+            headers={"authorization": secret},
+            files={"unlock": open(file, "rb")},
+        )
+        return True
+
+
+def get_lockfile_command(env, *args, **kwargs):
+    if not env["server"]:
+        print("no server set")
+    else:
+        server_key = env["server"]
+        server_data = env["server_data"][server_key]
+        url = server_data["url"]
+        secret = server_data["secret"]
+        target_file = args[0][0]
+        got = get(url + "/lockfile", headers={"authorization": secret})
+        with open(target_file, "wb") as f:
+            f.write(got.content)
+        return True
