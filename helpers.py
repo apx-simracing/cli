d4dd13b30ff42c4119b39d71ae83c39c2b8ff272
pranciskus <pranciskusjansas@gmail.com>
2022-02-17 20:58:17 +0200

    cleanup

--- helpers.py
+++ helpers.py
@@ -3,7 +3,7 @@ from os import path
 from os.path import isfile
 from json import load
 
-if __package__ is None or __package__ == '':
+if __package__ is None or __package__ == "":
     from commands.router import SHELL_COMMANDS
 else:
     from cli.commands.router import SHELL_COMMANDS
@@ -14,6 +14,7 @@ logger = logging.getLogger(__name__)
 ROOT_PATH = path.dirname(path.realpath(__file__))
 SERVERS_JSON_PATH = path.join(ROOT_PATH, "servers.json")
 
+
 def get_servers_data(path=SERVERS_JSON_PATH):
     if not isfile(path):
         logging.error(f"Servers file not found: {path}")
@@ -22,32 +23,31 @@ def get_servers_data(path=SERVERS_JSON_PATH):
         logging.debug(f"Reading file: {path}")
         return load(read_file)
 
-# TODO: WIP, ask @Günther Hubspecht [HTTP 410] for usage requirements - args and config???
+
 def run_apx_package_command(server_hash=None, command=None, args=None):
 
     if args is not None:
-        
+
         if not isinstance(args, list) or len(args) == 0:
-            msg = f'APX package command args need to be not emty list, got args={args}'
+            msg = f"APX package command args need to be not emty list, got args={args}"
             logging.error(msg)
             raise Exception(msg)
-        
-        # all cli commands expect args to be list of lists
-        # args = [args,]
 
     server_data = get_servers_data()
-    
+
     env = {"server_data": server_data, "server": server_hash, "server_config": None}
 
     if command not in SHELL_COMMANDS:
-        msg = f'APX package command not found {command}'
+        msg = f"APX package command not found {command}"
         logger.error(msg)
         raise Exception(msg)
 
     cli_command = SHELL_COMMANDS[command]
 
-    logger.info(f'APX package command running {command} server={server_hash} env={env} args={args}')        
-    
+    logger.info(
+        f"APX package command running {command} server={server_hash} env={env} args={args}"
+    )
+
     result = cli_command(env, args)
 
     if not result:
